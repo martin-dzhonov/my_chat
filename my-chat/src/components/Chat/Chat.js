f0ef606a5d3a4dc345519cd3013591a73c1231aa
martin-dzhonov <martin.dzhonov@gmail.com>
2020-01-23 04:07:39 -0800

    message box layout

--- my-chat/src/components/Chat/Chat.js
+++ my-chat/src/components/Chat/Chat.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Messages from '../Messages/Messages';
+import queryString from 'query-string';
 
 const Chat = ({ location }) => {
+
+    const [name, setName] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'http://localhost:5000';
+
+    useEffect(() => {
+        const { name } = queryString.parse(location.search);
+        setName(name)
+
+      }, [ENDPOINT, location.search]);
+
     return (
-        <div>Chat here</div>
+        <div className="outerContainer">
+            <div className="container">
+                <Messages messages={messages} name={name} />
+            </div>
+        </div>
       );
 }
 
