d28012d6f33181480acd6ec8c919f4136815e188
martin-dzhonov <martin.dzhonov@gmail.com>
2020-01-23 03:21:05 -0800

    routing, login layout

---
+++ my-chat/src/components/Chat/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Chat = ({ location }) => {
+    return (
+        <div>Chat here</div>
+      );
+}
+
+
+export default Chat;
---
+++ my-chat/src/components/Login/Login.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import './Login.css';
+
+export default function Login() {
+  const [name, setName] = useState('');
+
+  return (
+    <div className="loginOuterContainer">
+      <div className="loginInnerContainer">
+        <h1 className="heading">Welcome</h1>
+        <div>
+          <input placeholder="Name" className="loginInput" type="text" onChange={(event) => setName(event.target.value)} />
+        </div>
+        <Link onClick={e => (!name) ? e.preventDefault() : null} to={`/chat?name=${name}`}>
+          <button className={'button mt-20'} type="submit">Sign In</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
